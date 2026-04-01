eb256f6ba336b787eaf5d8fd6badc512869be39e
Mikhail Magradze <magradzemikhail@yandex.ru>
2026-04-01 12:04:24 +0300

    Add terraform static ip

---
+++ infra/outputs.tf
@@ -0,0 +1,3 @@
+output "external_ip" {
+  value = yandex_vpc_address.addr.external_ipv4_address[0].address
+}
--- infra/vm.tf
+++ infra/vm.tf
@@ -24,6 +24,7 @@ resource "yandex_compute_instance" "vm_1" {
   network_interface {
     subnet_id            = yandex_vpc_subnet.kittygram_subnet[0].id
     nat                  = var.nat
+    nat_ip_address       = yandex_vpc_address.addr.external_ipv4_address[0].address
     security_group_ids   = [yandex_vpc_security_group.kittygram_sg.id]
   }
 
