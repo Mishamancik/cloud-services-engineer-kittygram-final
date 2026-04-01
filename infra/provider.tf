4e2b27680a160aca14f125741006bb72b90b8f03
Mikhail Magradze <magradzemikhail@yandex.ru>
2026-04-01 15:36:07 +0300

    Fix workflow 5

--- infra/provider.tf
+++ infra/provider.tf
@@ -19,8 +19,8 @@ terraform {
     skip_requesting_account_id  = true
     skip_s3_checksum            = true
 
-    dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/ru-central1/b1gn0dch107j3ko8njsj/etn377d9gihnlfa4sdcn"
-    dynamodb_table    = "lock"
+    # dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/ru-central1/b1gn0dch107j3ko8njsj/etn377d9gihnlfa4sdcn"
+    # dynamodb_table    = "lock"
   }
 }
 
